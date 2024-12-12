3e97930bb1c9b4cd960d6deeb3f89d1585f3cf5d
ptr-s <rmsptr@yahoo.com>
2024-12-12 09:19:48 +0300

    append main code

---
+++ task_manager.py
@@ -0,0 +1,102 @@
+"""
+Менеджер задач
+Задача: Создай класс Task, который позволяет управлять задачами (делами).
+У задачи должны быть атрибуты: описание задачи, срок выполнения и статус
+(выполнено/не выполнено). Реализуй функцию для добавления задач, отметки
+выполненных задач и вывода списка текущих (не выполненных) задач.
+"""
+import datetime
+
+class Task():
+    def __init__(self, name: str, description: str, deadline: datetime.date):
+        self.name = name
+        self.description = description
+        self.deadline = deadline
+        self.completed = False
+
+    def complete(self):
+        self.completed = True
+
+    def info(self, separate = '\n'):
+        print(f"Имя: {self.name}{separate}"
+              f"Описание: {self.description}{separate}"
+              f"Срок выполнения: {self.deadline.strftime("%d-%m-%Y")}{separate}"
+              f"Задача {'' if self.completed else 'не '}выполнена")
+
+    def status(self):
+        print(f"Задача '{self.name}' {'' if self.completed else 'не '}выполнена")
+
+class TaskManager():
+    def __init__(self):
+        self.task_list = []
+
+    def add_task(self, name: str, description: str, deadline: datetime.date):
+        self.task_list.append(Task(name, description, deadline))
+
+    def complete(self, name: str):
+        result = None
+        for task_item in self.task_list:
+            if task_item.name == name:
+                task_item.complete()
+                result = task_item
+        return result
+
+    def remove_task(self, name: str):
+        i = 0
+        while i < len(self.task_list):
+            if self.task_list[i].name == name:
+                del self.task_list[i]
+                continue
+            i += 1
+
+    def remove_completed(self):
+        i = 0
+        while i < len(self.task_list):
+            if self.task_list[i].completed:
+                del self.task_list[i]
+                continue
+            i += 1
+
+    def show_current_tasks(self, all_task: bool = False):
+        for task_item in self.task_list:
+            if not task_item.completed or all_task:
+                task_item.info(', ')
+
+
+def main():
+    # создаём Менеджер задач
+    task_manager = TaskManager()
+
+    # добавляем задачи
+    task_manager.add_task("Позвонить родителям",
+                          "Позвонить родителям, спросить что купить",
+                          datetime.date.today())
+    task_manager.add_task("Разработать ТЗ",
+                          "Подготовить ТЗ по новому проекту",
+                          datetime.date.today() + datetime.timedelta(days=5))
+    task_manager.add_task("Провести переговоры",
+                          "Провести переговоры по новому проекту",
+                          datetime.date.today() + datetime.timedelta(days=7))
+    task_manager.add_task("Выполнить ДЗ",
+                          "Выполнить домашнее задание к уроку",
+                          datetime.date.today() + datetime.timedelta(days=1))
+    task_manager.add_task("Сходить в кино",
+                          "Сходить в кино на премьеру",
+                          datetime.date.today() + datetime.timedelta(days=3))
+
+    # выводим список задач
+    print("Активные задачи:")
+    task_manager.show_current_tasks()
+
+    # завершаем некоторые задачи
+    print("Завершаем две задачи.")
+    task_manager.complete("Позвонить родителям").status()
+    task_manager.complete("Выполнить ДЗ").status()
+
+    # выводим список задач
+    print("Активные задачи:")
+    task_manager.show_current_tasks()
+
+
+if __name__ == "__main__":
+    main()
